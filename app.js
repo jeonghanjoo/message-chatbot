032ac610ff278452933baf2ac691c554f12644e7
jeonghanjoo <joo3436@gmail.com>
2016-08-28 23:52:01 +0900

    attach image

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ const
   https = require('https'),
   request = require('request');
 
-  var app = express();
-  app.set('port', process.env.PORT || 5000);
-  app.use(bodyParser.json());
-  app.use(express.static('public'));
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-  const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
   process.env.MESSENGER_APP_SECRET : config.get('appSecret');
 
-  const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
   (process.env.MESSENGER_VALIDATION_TOKEN) : config.get('validationToken');
 
-  const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) : config.get('pageAccessToken');
 
-  if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
-    console.error("Missing config values");
-    process.exit(1);
-  }
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
 
 getStarted();
 
@@ -34,11 +34,11 @@ getStarted();
  * setup is the same token used here.
  */
 
-app.get('/webhook', function(req, res) {
+app.get('/webhook', function (req, res) {
 
-   if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
     res.status(200).send(req.query['hub.challenge']);
-   } else {
+  } else {
     console.error("Failed validation. wrong Token");
     res.sendStatus(403);
   }
@@ -57,10 +57,10 @@ app.post('/webhook', function (req, res) {
   if (data.object == 'page') {
     // Iterate over each entry
     // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
+    data.entry.forEach(function (pageEntry) {
 
       // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
+      pageEntry.messaging.forEach(function (messagingEvent) {
 
         // receive 라는 것은 봇의 관점에서 receive다.
         // 즉, 유저가 채팅을 시작했다는 의미이다.
@@ -115,7 +115,7 @@ function receivedAuthentication(event) {
  * then we'll simply confirm that we've received the attachment.
  */
 
- // 받는 메세지.(어떤 메세지가 페이스북 페이지에 보내졌을때 이 함수 이벤트가 불린다.)
+// 받는 메세지.(어떤 메세지가 페이스북 페이지에 보내졌을때 이 함수 이벤트가 불린다.)
 function receivedMessage(event) {
 
   var senderID = event.sender.id;
@@ -131,23 +131,22 @@ function receivedMessage(event) {
   if (messageText) {
     console.log('보낸 메세지 : ' + messageText);
 
-// 서버로 보내는 부분 추가.
+    // 서버로 보내는 부분 추가.
     var options = {
-  uri: 'http://mojitok.ap-northeast-2.elasticbeanstalk.com/recommend',
-  method: 'POST',
-  json: {
-    TEXT: messageText
-  }
-};
+      uri: 'http://mojitok.ap-northeast-2.elasticbeanstalk.com/recommend',
+      method: 'POST',
+      json: {
+        TEXT: messageText
+      }
+    };
 
-request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var first = body.EMOTICONS[0];
-      sendImageMessage(senderID, first);
-      sendTextMessage(senderID,"ok");
-  }
-});
-    } else if (messageAttachments) {
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var first = body.EMOTICONS[0];
+        sendImageMessage(senderID, first);
+      }
+    });
+  } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
   }
 }
@@ -168,12 +167,12 @@ function receivedDeliveryConfirmation(event) {
   var sequenceNumber = delivery.seq;
 
   if (messageIDs) {
-    messageIDs.forEach(function(messageID) {
+    messageIDs.forEach(function (messageID) {
       console.log("Received delivery confirmation for message ID: %s",
         messageID);
-      });
-    }
+    });
   }
+}
 
 /*
  * Postback Event
@@ -182,8 +181,8 @@ function receivedDeliveryConfirmation(event) {
  */
 function receivedPostback(event) {
   var senderID = event.sender.id;
-    sendTextMessage(senderID, "안녕하세요!!\n 저는 페이스북 봇입니다");
- }
+  sendTextMessage(senderID, "안녕하세요!!\n 저는 페이스북 봇입니다");
+}
 
 
 
@@ -192,7 +191,7 @@ function receivedPostback(event) {
  * Send a message with an using the Send API.
  *
  */
-function sendImageMessage(recipientId , firstimage) {
+function sendImageMessage(recipientId, firstimage) {
   var messageData = {
     recipient: {
       id: recipientId
@@ -201,7 +200,7 @@ function sendImageMessage(recipientId , firstimage) {
       attachment: {
         type: "image",
         payload: {
-          url: "https://s3.ap-northeast-2.amazonaws.com/mojitok-bucket/"+firstimage
+          url: "https://s3.ap-northeast-2.amazonaws.com/mojitok-bucket/" + firstimage
         }
       }
     }
@@ -211,8 +210,8 @@ function sendImageMessage(recipientId , firstimage) {
 
 function sendTextMessage(recipientId, messageText) {
   var messageData = {
-    recipient: {id: recipientId},
-    message: {text: messageText}
+    recipient: { id: recipientId },
+    message: { text: messageText }
   };
   callSendAPI(messageData);
 }
@@ -220,16 +219,46 @@ function sendTextMessage(recipientId, messageText) {
 
 function getStarted() {
   var messageData = {
-    "setting_type":"call_to_actions",
-    "thread_state":"new_thread",
-    "call_to_actions":[
-    {
-      "payload":"USER_DEFINED_PAYLOAD"
-    }
-  ]
+    "setting_type": "call_to_actions",
+    "thread_state": "new_thread",
+    "call_to_actions": [
+      {
+        "payload": "USER_DEFINED_PAYLOAD"
+      }
+    ]
   };
   callThreadAPI(messageData);
 }
+/**
+ *   -F recipient='{"id":"USER_ID"}' \
+  -F message='{"attachment":{"type":"image", "payload":{}}}' \
+  -F filedata=@/tmp/shirt.png \
+ * 
+ */
+function callUploadAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    form : {
+      recipient : {id : messageData.recipient.id},
+      message: {attachment : {type : "image", payload : {}}},
+      filedata: "./tmp/1.jpg"
+    }
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
 
 /*
  * Call the Send API. The message data goes in the body. If successful, we'll
@@ -279,7 +308,7 @@ function callThreadAPI(messageData) {
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid
 // certificate authority.
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 module.exports = app;
