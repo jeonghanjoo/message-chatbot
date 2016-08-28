66fcdce8337952b3c062af8ca2f63edac9f93110
jeonghanjoo <joo3436@gmail.com>
2016-08-29 01:24:33 +0900

    stream

--- app.js
+++ app.js
@@ -221,7 +221,7 @@ function uploadImageMessage(recipientId, imagePath) {
     formData : {
       recipient : {"id": recipientId},
       message: {"attachment": {"type" : "image", "payload" : {}}},
-      filedata: imagePath
+      filedata: fs.createReadStream(imagePath)
     }
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
